ffa49dcb9fb6c0ded310ed880e6ee7ab6a5d241f
DiptyMoniKha <72722700+DiptyMoniKha@users.noreply.github.com>
2020-11-24 01:08:58 +0600

    Final term lab1 complete
    Final term lab1 complete

---
+++ lab 1/Account/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Saving("dipty", "10101", 900);
+            a1.ShowInfo();
+            a1.Deposit(10101);
+            a1.Withdraw(1256);
+            a1.ShowInfo();
+            Console.WriteLine();
+            
+            Account a2 = new Fixed("dip", "56544", 6500, 45, 2020);
+            a2.ShowInfo();
+            a2.Deposit(500);
+            a2.Withdraw(6520);
+            a2.ShowInfo();
+
+        }
+    }
+}
