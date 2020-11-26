2ae9220b2c5e39aebdcf13e7793914cad1826c53
DiptyMoniKha <72722700+DiptyMoniKha@users.noreply.github.com>
2020-11-27 01:47:33 +0600

    Test Interface 1
    Test Interface 1

---
+++ Test Interface 1/Final second lab/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_second_lab
+{
+    class Calculator : CalculatorInterface
+    {
+        public Calculator()
+        {
+            Console.WriteLine("calculator.");
+        }
+        
+        public double SquareRoot(double x)
+        {
+            Console.WriteLine("Square Root : "+ Math.Sqrt(x));
+            return Math.Sqrt(x);
+        }
+
+        public int XtoY(int x, int y)
+        {
+            Console.WriteLine("X To Y : " + Convert.ToInt32(Math.Pow(x, y)));
+            return Convert.ToInt32(Math.Pow(x, y));
+        }
+
+       
+
+        
+    }
+}
---
+++ Test Interface 1/Final second lab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_second_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NormalCalculator n1 = new NormalCalculator();
+            n1.sum(10, 20);
+            n1.sub(40, 20);
+            n1.multiplication(15, 10);
+            n1.division(100, 20);
+            Console.WriteLine();
+            ScientificCalculator s1 = new ScientificCalculator();
+            s1.XtoY(5, 10);
+            s1.SquareRoot(8);
+            
+        }
+    }
+}
